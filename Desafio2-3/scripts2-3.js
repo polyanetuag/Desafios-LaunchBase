096ea4e18fa957bfeed90d7b45464487dfb55c14
PolyaneTuag <pollyportugues@hotmail.com>
2020-06-30 20:30:29 -0300

    Modal

---
+++ Desafio2-3/scripts2-3.js
@@ -0,0 +1,14 @@
+const modalOverlay = document.querySelector('.modal-overlay');
+const cards = document.querySelectorAll('.card')
+
+for (let card of cards) {
+    card.addEventListener("click", function() {
+        const courseId = card.getAttribute("id");
+        modalOverlay.classList.add('active')
+        modalOverlay.querySelector("iframe").src = `https://rocketseat.com.br/${courseId}`
+    })
+}
+
+document.querySelector(".close-modal").addEventListener("click", function() {
+    modalOverlay.classList.remove("active")
+})
